65098efc4fa60ec25d8ddd1140915bfd70fef969
Kevin Sarria <reysabueso256@gmail.com>
2023-08-13 20:33:45 -0500

    Seccion de estilos basicos con react native terminada

--- 02-MiPrimeraApp/App.tsx
+++ 02-MiPrimeraApp/App.tsx
@@ -1,8 +1,22 @@
-import { CounterScreen, HolaMundoScreen } from "./src/screens"
+import {SafeAreaView} from 'react-native';
+import {
+  BoxObjectModelScreen,
+  CounterScreen,
+  HolaMundoScreen,
+} from './src/screens';
+import { PositionScreen } from './src/screens/PositionScreen';
+import { FlexScreen } from './src/screens/FlexScreen';
+import { TareaScreen } from './src/screens/TareaScreen';
 
 export const App = () => {
   return (
-    // <HolaMundoScreen />
-    <CounterScreen />
-  )
-}
+    <SafeAreaView style={{ flex: 1 }}>
+       { /* <HolaMundoScreen /> */ }
+       { /* <CounterScreen /> */ }
+      {/* <BoxObjectModelScreen /> */}
+      {/* <PositionScreen /> */}
+      {/* <FlexScreen /> */}
+      <TareaScreen />
+    </SafeAreaView>
+  );
+};
